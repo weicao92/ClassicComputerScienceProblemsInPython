a05857b4003313fe7fa704c1f17bbd4d5f666713
weicao <weicao92@163.com>
2020-06-30 15:28:09 +0800

    add comment to encryption example of ch1

--- Chapter1/unbreakable_encryption.py
+++ Chapter1/unbreakable_encryption.py
@@ -21,7 +21,7 @@ def random_key(length: int) -> int:
     # generate length random bytes
     tb: bytes = token_bytes(length)
     # convert those bytes into a bit string and return it
-    return int.from_bytes(tb, "big")
+    return int.from_bytes(tb, "little")
 
 
 def encrypt(original: str) -> Tuple[int, int]:
@@ -34,6 +34,7 @@ def encrypt(original: str) -> Tuple[int, int]:
 
 def decrypt(key1: int, key2: int) -> str:
     decrypted: int = key1 ^ key2  # XOR
+    # avoid round error， 这个参数表示的是 “字节数”
     temp: bytes = decrypted.to_bytes((decrypted.bit_length() + 7) // 8, "big")
     return temp.decode()
 
